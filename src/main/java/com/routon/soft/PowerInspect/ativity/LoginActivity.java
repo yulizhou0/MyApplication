f0c757f6d74608efc89c4202693442c1e14810c2
yulizhou0 <18171406625@163.com>
2017-10-11 14:30:28 +0800

    1231

---
+++ src/main/java/com/routon/soft/PowerInspect/ativity/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.routon.soft.PowerInspect.ativity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.routon.soft.PowerInspect.R;
+import com.routon.soft.PowerInspect.base.BaseActivity;
+import com.routon.soft.PowerInspect.base.EMLayoutBinder;
+
+@EMLayoutBinder(R.layout.activity_login)
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
