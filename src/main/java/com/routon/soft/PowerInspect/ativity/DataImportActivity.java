0f650bacdd85dca8afdd13260af9e0f8793526db
yulizhou0 <18171406625@163.com>
2017-10-13 17:08:47 +0800

    1231

--- src/main/java/com/routon/soft/PowerInspect/ativity/DataImportActivity.java
+++ src/main/java/com/routon/soft/PowerInspect/ativity/DataImportActivity.java
@@ -14,31 +14,24 @@ import com.routon.soft.PowerInspect.view.GifView;
  * Created by Administrator on 2017/10/13.
  */
 @EMLayoutBinder(R.layout.activity_launch)
-public class LaunchActivity extends BaseActivity{
+public class DataImportActivity extends BaseActivity{
     @EMViewBinder(R.id.gif)
     private GifView gifView;
     private CustomDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        gifView.setMovieResource(R.raw.loading);
+        gifView.setMovieResource(R.raw.waitting);
         CustomDialog.Builder customBuilder = new
                 CustomDialog.Builder(this);
         customBuilder.setTitle(R.string.title)
-                .setMessage("发现xxx.xls，是否更新现有数据？")
+                .setMessage("xxx.xls第X行第X列数据错误")
                 .setNegativeButton(getString(R.string.update), new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
-                        //TODO 跳转到更新界面
+                        //TODO 退出系统
                     }
-                })
-                .setPositiveButton(getString(R.string.ok),
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                                //TODO 跳转到主界面
-                            }
-                        });
+                });
         dialog = customBuilder.create();
         dialog.show();
     }
